e79e7a6f00d631dddcee4a0b09913562a5922f12
Bamdo01 <qjatncc1128@gmail.com>
2024-07-17 15:33:14 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace NUPROJECT_vcs
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnFriendListItemClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new FirstPage();
+        }
+
+    }
+}
